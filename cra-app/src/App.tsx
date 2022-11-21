6c9f3df43bb8603c8b4e8f08c08047caa715d119
alican <alicanerdurmaz@gmail.com>
2022-11-21 03:07:10 +0300

    first commit

--- cra-app/src/App.tsx
+++ cra-app/src/App.tsx
@@ -1,17 +1,14 @@
-import React from "react";
+import React from 'react'
 
-import { Refine } from "@pankod/refine-core";
+import { Refine } from '@pankod/refine-core'
 
-import dataProvider from "@pankod/refine-simple-rest";
-import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from '@pankod/refine-simple-rest'
+import routerProvider from '@pankod/refine-react-router-v6'
 
 function App() {
   return (
-    <Refine
-      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-      routerProvider={routerProvider}
-    />
-  );
+    <Refine dataProvider={dataProvider('https://api.fake-rest.refine.dev') as any} routerProvider={routerProvider} />
+  )
 }
 
-export default App;
+export default App
